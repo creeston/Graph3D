ed00ec90fa0ecba6c9d942e231320963d854cb04
creeston <mityy2012@gmail.com>
2015-05-09 19:49:45 +0300

    our header file with function prototypes, defines and structures

---
+++ test_header.h
@@ -0,0 +1,57 @@
+#define max2(x,y) ((x) > (y) ? (x) : (y))
+#define min2(x,y) ((x) < (y) ? (x) : (y))
+#define max3(x,y,z) ((x) > (y) ? max2(x,z) : max2(y,z))
+#define min3(x,y,z) ((x) < (y) ? min2(x,z) : min2(y,z))
+#define xwhole(x) ((int)(((x) - Xmin) / deltaX))
+#define ywhole(y) ((int)(((y) - Ymin) / deltaY))
+#define xreal(i) (Xmin + ((i)) * deltaX)
+
+#define M -1000000.0
+#define NVERTEX 3000
+#define NTRIANGLE 200
+#define NSCREEN 40
+#define BIG 1e30
+#define NPOLY 400
+#define NNTRSET 200
+
+#define eps 1e-5
+#define meps -1e-5
+#define oneminus (1 - 1e-5)
+#define oneplus (1 + 1e-5)
+
+struct ver {
+    double x,y,z;
+    int *connect;
+};
+
+struct tr{
+    int A,B,C;
+    double a,b,c,h;
+};
+
+struct node {
+    int jtr;
+    struct node *next;
+};
+
+struct scr{
+    int tr_cov;
+    double tr_dist;
+    struct node *start;
+};
+
+
+void linesegment(double xP, double yP, double zP, double xQ, double yQ, double zQ, int k0);
+void add_linesegment(int P, int Q);
+int counter_clock(int i0, int i1, int i2, double *pdist, int code);
+int reflo(double *px);
+int reint(int *pi);
+void skipbl();
+void error(char *string);
+void coeff(double rho, double theta, double phi);
+void viewing(double x, double y, double z, double *pxe, double *pye, double *pze);
+void init_viewport(double *pXMIN, double *pXMAX, double *pYMIN, double *pYMAX0);
+void opengraph();
+void check();
+void drawtr();
+
